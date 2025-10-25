ed101da35137f97cfc122acc944887f2cbf52221
Chayan <chayan.surana3@gmail.com>
2025-10-25 11:32:26 +0530

    new changes to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,80 @@
 import React from "react";
-import bg1 from "../images/pattern-bg-mobile.png";
-import bg2 from "../images/pattern-bg-desktop.png";
+import bgMobile from "../images/pattern-bg-mobile.png";
+import bgDesktop from "../images/pattern-bg-desktop.png";
 
-function Header(props) {
+function Header({ handleSubmit, spinner }) {
     return (
-        <div>
-            <div className="relative flex flex-col items-center justify-center lg:hidden" style={{ backgroundImage: `url(${bg1})`, backgroundSize: 'cover' }}>
-                <h1 className="text-center text-white text-4xl p-6 font-normal font-bold">IP Address Tracker</h1>
-                <div className="flex mb-12">
-                    <input id="ip-input1" className="p-2 mr-0.5 border-black border-4" placeholder="For Example: 8.8.8.8" />
-                    <button className="bg-black text-white px-4 py-2" onClick={() => { props.handleSubmit("ip-input1") }}>Search</button>
-                </div>
-                <div role="status" id="spinner" className={props.spinner ? "mx-auto" : "mx-auto hidden"}>
-                    <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                    </svg>
-                    <span class="sr-only">Loading...</span>
-                </div>
-            </div>
-            <div className="lg:flex relative flex-col items-center justify-center hidden h-60" style={{ backgroundImage: `url(${bg2})`, backgroundSize: 'cover' }}>
-                <h1 className="text-center text-white text-4xl p-8 font-normal2 font-bolder">IP Address Tracker</h1>
-                <div className="flex mb-16 justify-center">
-                    <input id="ip-input2" className="focus:mr-0.5 px-4 w-96 rounded-lg rounded-tl-2xl rounded-bl-2xl py-3 rounded-r-none" placeholder="For Example: 8.8.8.8" />
-                    <button className="bg-black text-white px-4 py-2" onClick={() => props.handleSubmit("ip-input2")}>Search</button>
-                </div>
-                <div role="status" id="spinner" className={props.spinner ? "mx-auto" : "mx-auto hidden"}>
-                    <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                    </svg>
-                    <span class="sr-only">Loading...</span>
-                </div>
+        <div
+            className="relative flex flex-col items-center justify-center h-60 lg:h-64 bg-cover bg-center"
+            style={{
+                backgroundImage: `url(${window.innerWidth < 1024 ? bgMobile : bgDesktop})`,
+            }}
+        >
+            {/* Title */}
+            <h1 className="text-center text-white text-3xl lg:text-4xl font-bold p-6">
+                IP Address Tracker
+            </h1>
+
+            {/* Input + Button */}
+            <div className="flex mb-10 justify-center w-full px-4">
+                <input
+                    id="ip-input"
+                    className="flex-1 max-w-xs lg:max-w-md px-4 py-3 rounded-l-2xl border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-black"
+                    placeholder="For Example: 8.8.8.8"
+                />
+                <button
+                    onClick={() => handleSubmit("ip-input")}
+                    className="bg-black text-white px-5 py-3 rounded-r-2xl hover:bg-gray-800 transition-all"
+                >
+                    Search
+                </button>
             </div>
 
+            {/* Spinner */}
+            <div
+                role="status"
+                className={`${spinner ? "block" : "hidden"} mx-auto`}
+            >
+                <svg
+                    aria-hidden="true"
+                    className="w-8 h-8 text-gray-200 animate-spin fill-blue-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M100 50.5908C100 78.2051 77.6142 
+               100.591 50 100.591C22.3858 100.591 
+               0 78.2051 0 50.5908C0 22.9766 22.3858 
+               0.59082 50 0.59082C77.6142 0.59082 
+               100 22.9766 100 50.5908ZM9.08144 
+               50.5908C9.08144 73.1895 27.4013 
+               91.5094 50 91.5094C72.5987 91.5094 
+               90.9186 73.1895 90.9186 50.5908C90.9186 
+               27.9921 72.5987 9.67226 50 9.67226C27.4013 
+               9.67226 9.08144 27.9921 9.08144 
+               50.5908Z"
+                        fill="currentColor"
+                    />
+                    <path
+                        d="M93.9676 39.0409C96.393 38.4038 
+               97.8624 35.9116 97.0079 33.5539C95.2932 
+               28.8227 92.871 24.3692 89.8167 20.348C85.8452 
+               15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 
+               4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 
+               0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 
+               1.69328 37.813 4.19778 38.4501 6.62326C39.0873 
+               9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 
+               9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 
+               10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 
+               17.9648 79.3347 21.5619 82.5849 25.841C84.9175 
+               28.9121 86.7997 32.2913 88.1811 35.8758C89.083 
+               38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                    />
+                </svg>
+                <span className="sr-only">Loading...</span>
+            </div>
         </div>
     );
 }
